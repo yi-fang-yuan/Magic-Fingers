e5c3092a0c3436442a64d1b2f23473e8856367de
Alexander Li <alexander.wd.li@gmail.com>
2019-11-17 04:16:11 -0500

    Restructured and added py files
    Added dir containing all json files parsed as well as python files for the NN and main

---
+++ nn.py
@@ -0,0 +1,104 @@
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import optimizers
+import json
+import os
+import numpy
+
+files = os.listdir('./jsonfiles')
+dataset = list()
+alphas = list()
+for f in files:
+    if f[5] == 'A':
+        alphas.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'B':
+        alphas.append([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'C':
+        alphas.append([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'D':
+        alphas.append([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'E':
+        alphas.append([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'F':
+        alphas.append([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'G':
+        alphas.append([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'H':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'I':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'J':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'K':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'L':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'M':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'N':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'O':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'P':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'Q':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'R':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'S':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'T':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+    elif f[5] == 'U':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+    elif f[5] == 'V':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+    elif f[5] == 'W':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+    elif f[5] == 'X':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+    elif f[5] == 'Y':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+    elif f[5] == 'Z':
+        alphas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+
+    with open('./jsonfiles/' + f) as json_file1:
+        data = json.load(json_file1)
+        if list(data["frames"][0]["persons"][0]["hand_pose"].keys())[0] == 'right':
+            dataset.append(
+                numpy.array([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                             -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1] +
+                            data["frames"][0]["persons"][0]["hand_pose"]["right"]["joints"]))
+        elif len(list(data["frames"][0]["persons"][0]["hand_pose"].keys())) == 1:
+            dataset.append(numpy.array(data["frames"][0]["persons"][0]["hand_pose"]["left"]["joints"] +
+                                       [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                                        -1,
+                                        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                                        -1]))
+        else:
+            dataset.append(numpy.array(data["frames"][0]["persons"][0]["hand_pose"]["left"]["joints"] +
+                                       data["frames"][0]["persons"][0]["hand_pose"]["right"]["joints"]))
+
+model = Sequential()
+model.add(Dense(128, input_dim=84, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(26, activation='softmax'))
+
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+model.fit(numpy.array(dataset), numpy.array(alphas), epochs=500, batch_size=25)
+
+print(model.evaluate(numpy.array(dataset), numpy.array(alphas)))
+
+# Prediction Example
+# for prediction in model.predict(numpy.array(dataset)):
+#     print(numpy.argmax(prediction))
+
+model_json = model.to_json()
+
+with open("model.json", "w") as json_file2:
+    json_file2.write(model_json)
+model.save_weights("model.h5")
